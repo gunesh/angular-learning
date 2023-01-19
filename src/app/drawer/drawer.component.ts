d11c3fc7b381cfb9c3f214b4ea9c920dd0b1bc5e
gunesh <gunesh26@gmail.com>
2023-01-19 14:47:37 +0530

    Initial commit

---
+++ src/app/drawer/drawer.component.ts
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Drawer } from 'adintel-lib-poc';
+
+@Component({
+  selector: 'app-drawer',
+  template: '<div id="drawer"></div>',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DrawerComponent
+  implements OnInit, OnChanges, AfterViewInit, OnDestroy
+{
+  public rootId = 'drawer';
+  @Input() menuItems = [];
+  @Input() menuLocation = {};
+  @Input() showMenuBar: boolean;
+  @Output() onHandleSubMenuClick = new EventEmitter();
+
+  ngOnInit(): void {}
+
+  ngOnChanges() {
+    this.render();
+  }
+
+  ngAfterViewInit(): void {
+    this.render();
+  }
+
+  ngOnDestroy(): void {}
+
+  public onSubMenuClick = (key) => {
+    this.onHandleSubMenuClick.emit(key);
+  };
+
+  private render() {
+    ReactDOM.render(
+      React.createElement(Drawer, {
+        menuItems: this.menuItems,
+        menuLocation: this.menuLocation,
+        onSubMenuClick: this.onSubMenuClick,
+        showMenuBar: this.showMenuBar,
+      }),
+      document.getElementById(this.rootId)
+    );
+  }
+}
