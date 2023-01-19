254f014165ca214ca9e2745cb79da424b481a69d
gunesh <gunesh26@gmail.com>
2023-01-19 15:32:10 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,10 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private router: Router) {}
-  name = 'Gunesh';
-  menuItems = [
-    {
-      key: 'classification/#',
-      label: 'Classification',
-      icon: 'tasks',
-      value: 5,
-      submenus: [
-        {
-          key: 'classification',
-          label: 'Digital Mapping',
-          value: 6,
-          submenus: null,
-        },
-        {
-          key: 'classification/#/digital-classification',
-          label: 'Classification',
-          value: 7,
-          submenus: null,
-        },
-      ],
-    },
-    {
-      key: 'ingestion/#',
-      label: 'Ingestion',
-      icon: 'archive',
-      value: 11,
-      submenus: [
-        {
-          key: 'ingestion/#/dashboard/adThenaIngestion',
-          label: 'Dashboard',
-          value: 12,
-          submenus: null,
-        },
-        {
-          key: 'ingestion/#/summaryView',
-          label: 'Ingestion Summary',
-          value: 13,
-          submenus: null,
-        },
-        {
-          key: 'ingestion/#/manualIngestion',
-          label: 'Declarative Files',
-          value: 14,
-          submenus: null,
-        },
-      ],
-    },
-  ];
-
-  showMenuBar = false;
-  onSubMenuClick(key) {
-    // this.router.navigate([key]);
-    this.router.navigateByUrl(key);
-  }
+  name = 'Angular ' + VERSION.major;
 }
