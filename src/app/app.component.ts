ce774004c1223ea58fd35098b1f7cf1f400ccb06
gunesh <gunesh26@gmail.com>
2023-02-23 19:49:08 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,18 @@ export class AppComponent {
   ngOnInit() {
     console.log('ngOnInit:');
   }
+  columns = [
+    { title: 'Adı', field: 'name' },
+    { title: 'Soyadı', field: 'surname' },
+    { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+    {
+      title: 'Doğum Yeri',
+      field: 'birthCity',
+      lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+    },
+  ];
+  data = [{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }];
+  title = 'My Table';
   menuItems = [
     {
       key: 'classification/#',
