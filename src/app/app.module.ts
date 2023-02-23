2e2049a7963171bca9b47e1bafede397479d7609
gunesh <gunesh26@gmail.com>
2023-02-23 19:45:57 +0530

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DrawerComponent } from './drawer.component';
+import { MaterialTableComponent } from './mtable.component';
+
 import { ReactSearchComponent } from './search.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent,DrawerComponent,ReactSearchComponent],
+  declarations: [AppComponent, HelloComponent,DrawerComponent,ReactSearchComponent,MaterialTableComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/mtable.component.ts
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Router } from '@angular/router';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import MaterialTable from 'material-table';
+
+@Component({
+  selector: 'app-table',
+  template: '<div id="table"></div>',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MaterialTableComponent
+  implements OnInit, OnChanges, AfterViewInit, OnDestroy
+{
+  public rootId = 'table';
+  @Input() columns = [];
+  @Input() data = [];
+  @Input() title = 'Test table';
+  @Output() onHandleSubMenuClick = new EventEmitter();
+
+  constructor(private router: Router) {
+    console.log('Constructor:');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit:');
+  }
+
+  ngOnChanges() {
+    this.render();
+  }
+
+  ngAfterViewInit(): void {
+    this.render();
+  }
+
+  ngOnDestroy(): void {}
+
+  public onSubMenuClick = (key) => {
+    this.onHandleSubMenuClick.emit(key);
+  };
+
+  private render() {
+    ReactDOM.render(
+      React.createElement(MaterialTable, {
+        columns: this.columns,
+        data: this.data,
+        title: this.title,
+      }),
+      document.getElementById(this.rootId)
+    );
+  }
+}
