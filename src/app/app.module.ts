c8c35faa917a38bd08e12f2b99b2a1ca9731960f
gunesh <gunesh26@gmail.com>
2023-02-24 11:11:17 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,7 +11,7 @@ import { MaterialTableComponent } from './mtable.component';
 import { ReactSearchComponent } from './search.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule,AppRoutingModule, FormsModule],
   declarations: [AppComponent, HelloComponent,DrawerComponent,ReactSearchComponent,MaterialTableComponent],
   bootstrap: [AppComponent],
 })
