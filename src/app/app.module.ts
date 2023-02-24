493ea20de1cbb3ab603dd545a142342399bd4aed
gunesh <gunesh26@gmail.com>
2023-02-24 14:29:40 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
