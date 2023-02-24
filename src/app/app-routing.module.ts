67268e6ca97a06e22c9a31ac60e1dcce063e1f89
gunesh <gunesh26@gmail.com>
2023-02-24 11:09:27 +0530

    1 file modified, 9 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TestComponent } from './test/test.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ExampleComponent } from './example/example.component';
 
 const routes: Routes = [
   { path: '', component: ExampleComponent },
-  { path: 'search', component: ReactSearchComponent },
+  { path: 'search', component: ExampleComponent },
   { path: 'classification', component: TestComponent },
   {
-    path: '**', pathMatch: 'full',
-    component: PagenotfoundComponent
+    path: '**',
+    pathMatch: 'full',
+    component: PagenotfoundComponent,
   },
 ];
 
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
