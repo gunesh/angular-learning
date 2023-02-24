b7a129024ac00a2e57ce14f23852bcda6e07c85a
gunesh <gunesh26@gmail.com>
2023-02-24 11:36:25 +0530

    1 file modified, 4 added, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { DrawerComponent } from './drawer.component';
-import { MaterialTableComponent } from './mtable.component';
+import { HelloComponent } from './hello/hello.component';
+import { DrawerComponent } from './drawer/drawer.component';
+import { MaterialTableComponent } from './mtable/mtable.component';
 
-import { ReactSearchComponent } from './search.component';
+import { ReactSearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [BrowserModule,AppRoutingModule, FormsModule],
